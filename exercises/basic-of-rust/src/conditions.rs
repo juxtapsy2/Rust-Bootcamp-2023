92cfcfa2ff84609e21ad05f68e0c381ac44640d8
juxtapsy2 <frost.death.ap@gmail.com>
2023-07-05 12:20:11 +0700

    Finish conditions.rs

--- exercises/basic-of-rust/src/conditions.rs
+++ exercises/basic-of-rust/src/conditions.rs
@@ -4,26 +4,39 @@
 // - another function call
 // - additional variables
 pub fn bigger(a: i32, b: i32) -> i32 {
-    todo!()
+    if a > b {
+        a
+    }
+    else {
+        b
+    }
 }
 
 //Exercise 2
 // Input: Provide an arbitrary value of number
 // Check number is Positive or Negative or Zero
 // Output: &str
-fn check_number(number: u32) -> &'static str {
-    todo!()
+fn check_number(number: i32) -> &'static str {
+    if number > 0 {
+        return "Positive";
+    }
+    else if number < 0 {
+        return "Negative";
+    }
+    else {
+        return "Zero";
+    }
 }
 
 // Exercise 3
 // Step 1: Make me compile!
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
 
-pub fn foo_if_fizz(fizzish: &str) -> &str {
-    if fizzish == "fizz" {
-        "foo"
-    } else {
-        1
+pub fn foo_if_fizz(fizzish: &str) -> &'static str {
+    match fizzish {
+        "fizz" => return "foo",
+        "fuzz" => return "bar",
+        _ => return "baz"
     }
 }
 
@@ -31,14 +44,22 @@ pub fn foo_if_fizz(fizzish: &str) -> &str {
 // Determine if a given year is a leap year
 // Implement logic
 fn is_leap_year(year: i32) -> bool {
-    todo!()
+    if year % 4 == 0 && year % 100 != 0 || year % 400 == 0 {
+        return true;
+    }
+    return false;
 }
 
 // Exercise 5
 // Calculate the factorial of a number
 // Implement logic
 fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut fac:u32 = 1;
+    for i in 2..(n+1){
+         fac *= i;
+    }
+    return fac;
+    //factorial(n)
 }
 
 // Exercise 6
@@ -46,9 +67,16 @@ fn factorial(n: u32) -> u32 {
 // Implement logic
 
 fn is_prime(n: u32) -> bool {
-    todo!()
+    if n <= 1 {
+        return false;
+    }
+    for i in 2..n {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    return true;
 }
-
 
 // Don't mind this for now :)
 #[cfg(test)]
